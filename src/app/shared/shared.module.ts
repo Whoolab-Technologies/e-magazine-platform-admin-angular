314322512da0b0644bc32173b1b6b75cdb35da19
vishnu <vishnu@whoolab.com>
2023-05-20 10:34:55 +0530

    -import missing components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -27,8 +27,8 @@ import { ActionComponent } from './action/action.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
-
-
+import { LoaderComponent } from './loader/loader.component';
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
 @NgModule({
   declarations: [
     SidebarComponent,
@@ -36,6 +36,7 @@ import { ConfirmationComponent } from './confirmation/confirmation.component';
     LayoutComponent,
     ActionComponent,
     ConfirmationComponent,
+    LoaderComponent
   ],
   imports: [
     HeaderModule,
@@ -47,11 +48,11 @@ import { ConfirmationComponent } from './confirmation/confirmation.component';
     MatMenuModule,
     MatSelectModule,
     FormsModule,
-    MatSidenavModule,
-    MatDialogModule, FlexLayoutModule
+    MatSidenavModule, MatProgressSpinnerModule,
+    MatDialogModule, FlexLayoutModule, MatProgressBarModule
   ],
   exports: [
-    HeaderModule, ActionComponent,
+    HeaderModule, ActionComponent, MatProgressSpinnerModule, LoaderComponent,
     SidebarComponent, FormsModule, MatIconModule, MatDialogModule,
     FooterComponent, MatSidenavModule, FlexLayoutModule, MatButtonModule,
     LayoutComponent, MatCardModule, MatProgressBarModule, MatToolbarModule,
